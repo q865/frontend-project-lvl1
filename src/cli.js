c822fed4701e217aa4ac0ba12ce72777aa19883a
q865 <men865@gmail.com>
2020-10-26 02:37:58 +0300

    new file cli

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import promptly from "promptly";
+
+export default async () => {
+  console.log("Welcom to the Brain Games!");
+  const name = await promptly.prompt("May i have your Name?");
+  console.log(`Hello ${name}`);
+};
