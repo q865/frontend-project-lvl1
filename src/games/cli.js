15cedfe04de2d0d933503c583f7c8bd6fa71f20f
Sergey Panin <men865@gmail.com>
2021-09-28 16:27:53 +0300

    fix brain-games

--- src/games/cli.js
+++ src/games/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync'
 
 export default async (rules) => {
   console.log("Welcome to the Brain Games!");
-  const name = await readlineSync.question("May I have your Name? ");
+  const name = await readlineSync.question("May I have your name? ");
   console.log(`Hello, ${name}!`);
   rules ? console.log(rules) : ''
   return name
